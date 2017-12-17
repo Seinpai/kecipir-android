0fc4db569df93279f8e5d01332e83531c48dc0f1
Seinpai <hiz.husein@gmail.com>
2017-12-17 18:14:30 +0700

    Disabled Date Function

---
+++ app/src/main/java/com/kecipir/petani/rest/AppConfig.java
@@ -0,0 +1,8 @@
+package com.kecipir.petani.rest;
+
+/**
+ * Created by husein on 28/11/17.
+ */
+
+public class AppConfig {
+}
---
+++ app/src/main/java/com/kecipir/petani/rest/AppController.java
@@ -0,0 +1,11 @@
+package com.kecipir.petani.rest;
+
+import android.app.Application;
+
+/**
+ * Created by husein on 28/11/17.
+ */
+
+public class AppController extends Application {
+
+}
